9c9b7c6b7f113d74e357b1437cc417565dec49b9
Julian Santiago <j.santiago.rojo.conde@gmail.com>
2025-09-03 22:47:46 -0600

    Actualizacion ejercicio extra

--- app.js
+++ app.js
@@ -8,4 +8,21 @@ document.getElementById("enviar").addEventListener("click", function() {
 
     document.getElementById("resultado").innerText =
         `Hola ${nombre}, tu edad es ${edad} y ya estás aprendiendo ${lenguaje}.`;
+
+     document.getElementById("pregunta").innerText =`¿Te gusta estudiar ${lenguaje}?`;
+//Esta parte esconde y muestra los botones de Si y No
+    document.getElementById("Si").style.display = "inline-block";
+    document.getElementById("no").style.display = "inline-block";
+    document.getElementById("Respuesta").style.display = "block";
+     
+     document.getElementById("Si").addEventListener("click", function() {
+       document.getElementById("Respuesta").innerText =
+        `¡Muy bien! Sigue estudiando y tendrás mucho éxito.`;
+       
+  })
+       document.getElementById("no").addEventListener("click", function() {
+       document.getElementById("Respuesta").innerText =
+        ` Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?`;
+   })
 });
+
